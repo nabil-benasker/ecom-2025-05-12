ceecb5204eae6fc98301626a39068256bb0f51e5
ben asker <nabil.benasker@gmail.com>
2025-05-13 00:32:48 +0200

    customer-microservice

---
+++ customer-service/src/main/java/net/benasker/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,29 @@
+package net.benasker.customerservice;
+
+import net.benasker.customerservice.entities.Customer;
+import net.benasker.customerservice.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(CustomerRepository customerRepository) {
+        return args -> {
+
+            customerRepository.save(Customer.builder().
+                    name("nabil").email("nabil.benasker@gmail.com").build());
+            customerRepository.save(Customer.builder().name("beya").email("beya.benasker@gmail.com").build());
+			customerRepository.findAll().forEach(System.out::println);
+        };
+    }
+
+
+}
